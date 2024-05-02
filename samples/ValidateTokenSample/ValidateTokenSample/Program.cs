473a8f71494421f06a7d27fceb7d0e97ecd39f7e
vineshparamasivam <vinesh_paramasivam@trimble.com>
2024-05-02 11:00:22 +0530

    validatetokensample included

---
+++ samples/ValidateTokenSample/ValidateTokenSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Trimble.ID;
+
+
+IKeySetProvider keysetProvider = new OpenIdKeySetProvider(OpenIdEndpointProvider.Production);
+
+// Create a valiate claimset provider
+IClaimsetProvider claimsetProvider = new ValidatedClaimsetProvider(keysetProvider)
+                                            .WithOptions(new TokenValidationOptions{ ClockSkew = TimeSpan.FromMinutes(5)});
+
+// Retrieve the claimset
+IDictionary<string, object> claimset = await claimsetProvider.RetrieveClaimset("<ID-TOKEN>");
+
+foreach (var claim in claimset)
+{
+    Console.WriteLine($"{claim.Key}: {claim.Value}");
+}
+
+
+
